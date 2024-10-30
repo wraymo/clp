6e57b69bad5407539d4e56ce7c5686f7e24cf90e
gibber9809 <devinbook1@gmail.com>
2024-10-30 16:10:51 +0000

    Add comments to new classes

--- components/core/src/clp/CheckpointReader.hpp
+++ components/core/src/clp/CheckpointReader.hpp
@@ -4,6 +4,12 @@
 #include "ReaderInterface.hpp"
 
 namespace clp {
+/**
+ * CheckpointReader is a ReaderInterface designed to wrap other ReaderInterfaces, and prevent users
+ * from reading beyond a certain point in the underlying ReaderInterface. This can help prevent
+ * needing to seek backwards in the underlying ReaderInterface by setting a checkpoint at the start
+ * of the next section to be read.
+ */
 class CheckpointReader : public ReaderInterface {
 public:
     explicit CheckpointReader(ReaderInterface* reader, size_t checkpoint)
@@ -17,16 +23,43 @@ public:
         }
     }
 
+    // Methods implementing the ReaderInterface
+    /**
+     * Tries to get the current position of the read head in the underlying reader.
+     * @param pos Position of the read head in the underlying reader
+     * @return ErrorCode_Success on success
+     * @return ErrorCode_errno on failure
+     */
     ErrorCode try_get_pos(size_t& pos) override { return m_reader->try_get_pos(pos); }
 
+    /**
+     * Tries to seek to the given position, limited by the checkpoint. If pos is past the checkpoint
+     * then this reader seeks up to the checkpoint in the underlying stream, then returns
+     * ErrorCode_EndOfFile on success or ErrorCode_errno otherwise.
+     * @param pos
+     * @return ErrorCode_Success on success
+     * @return ErrorCode_EndOfFile on EOF or if trying to seek beyond the checkpoint
+     * @return ErrorCode_errno on failure
+     */
     ErrorCode try_seek_from_begin(size_t pos) override;
 
+    /**
+     * Tries to read up to a given number of bytes from the file, limited by the checkpoint. If the
+     * requested read would advance the read head beyond the checkpoint then the read size is
+     * adjusted such that the read head is advanced to the checkpoint. If a read is attempted when
+     * the read head is at the checkpoint then ErrorCode_EndOfFile is returned.
+     * @param buf
+     * @param num_bytes_to_read The number of bytes to try and read
+     * @param num_bytes_read The actual number of bytes read
+     * @return ErrorCode_errno on error
+     * @return ErrorCode_EndOfFile on EOF or trying to read after hitting checkpoint
+     * @return ErrorCode_Success on success
+     */
     ErrorCode try_read(char* buf, size_t num_bytes_to_read, size_t& num_bytes_read) override;
 
     ErrorCode
     try_read_to_delimiter(char delim, bool keep_delimiter, bool append, std::string& str) override {
-        // TODO: implement later if necessary
-        return ErrorCode_Failure;
+        return ErrorCode_Unsupported;
     }
 
 private:
--- components/core/src/clp_s/ArchiveReaderAdaptor.hpp
+++ components/core/src/clp_s/ArchiveReaderAdaptor.hpp
@@ -15,6 +15,10 @@
 #include "ZstdDecompressor.hpp"
 
 namespace clp_s {
+/**
+ * ArchiveReaderAdaptor is an adaptor class which helps with reading single and multi-file archives
+ * which exist on either S3 or a locally mounted file system.
+ */
 class ArchiveReaderAdaptor {
 public:
     class OperationFailed : public TraceableException {
@@ -32,10 +36,30 @@ public:
 
     ~ArchiveReaderAdaptor();
 
+    /**
+     * Load metadata for an archive including the header and metadata section. This method must be
+     * invoked before checking out any section of an archive, or calling `get_timestamp_dictionary`.
+     * @return ErrorCodeSuccess on success
+     * @return ErrorCode_errno on failure
+     */
     ErrorCode load_archive_metadata();
 
+    /**
+     * Checkout a reader for a given section of the archive. Reader must be checked back in with the
+     * `checkin_reader_for_section` method.
+     * @param section
+     * @return A ReaderInterface opened and pointing to the requested section.
+     * @throw OperationFailed if a reader is already checked out, or checking out this section would
+     *        force a backwards seek.
+     */
     clp::ReaderInterface& checkout_reader_for_section(std::string_view section);
 
+    /**
+     * Checkin a reader for a given section of the archive.
+     * @param section
+     * @throw OperationFailed if no reader is checked out, or if the section being checked in does
+     *        not match the section currently checked out.
+     */
     void checkin_reader_for_section(std::string_view section);
 
     std::shared_ptr<TimestampDictionaryReader> get_timestamp_dictionary() {
@@ -49,7 +73,6 @@ private:
 
     ErrorCode try_read_timestamp_dictionary(ZstdDecompressor& decompressor, size_t size);
 
-    // TODO: change this to metadata option
     bool m_single_file_archive{false};
     std::string m_path;
     ArchiveFileInfoPacket m_archive_file_info{};
@@ -58,7 +81,6 @@ private:
     std::optional<std::string> m_current_reader_holder;
     std::shared_ptr<TimestampDictionaryReader> m_timestamp_dictionary;
 
-    // TODO: switch to readerinterface
     std::shared_ptr<clp::ReaderInterface> m_reader;
     clp::CheckpointReader m_checkpoint_reader{nullptr, 0};
     InputOption m_input_config{};
